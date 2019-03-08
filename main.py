1f544c187c5745c2207312e16ac3d7fa6fa08799
Jordan Dashel <jordan.dashel@gmail.com>
2019-03-07 23:19:44 -0500

    adding new pages for new jordans -- dev, sax, va jordan html, css

--- main.py
+++ main.py
@@ -5,3 +5,27 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/dev-jordan')
+def dev_jordan():
+    return render_template('dev_jordan.html')
+
+@app.route('/my-dev-work')
+def my_dev_work():
+    return render_template('my_dev_work.html')
+
+@app.route('/sax-jordan')
+def sax_jordan():
+    return render_template('sax_jordan.html')
+
+@app.route('/my-sax-work')
+def my_sax_work():
+    return render_template('my_sax_work.html')
+
+@app.route('/va-jordan')
+def va_jordan():
+    return render_template('va_jordan.html')
+
+@app.route('/my-visual-work')
+def my_visual_work():
+    return render_template('my_visual_work.html')
